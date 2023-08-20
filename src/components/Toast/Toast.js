643dacfea74e3d1e1929c8b335055aafed3b24ea
catharygr <103951447+catharygr@users.noreply.github.com>
2023-08-20 15:28:38 +0100

    Uso de switch para manipular iconos

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -11,21 +11,34 @@ import VisuallyHidden from "../VisuallyHidden";
 
 import styles from "./Toast.module.css";
 
-const ICONS_BY_VARIANT = {
-  notice: Info,
-  warning: AlertTriangle,
-  success: CheckCircle,
-  error: AlertOctagon,
-};
+// const ICONS_BY_VARIANT = {
+//   notice: Info,
+//   warning: AlertTriangle,
+//   success: CheckCircle,
+//   error: AlertOctagon,
+// };
+
+function handleIcon(variant) {
+  switch (variant) {
+    case "notice":
+      return <Info size={24} />;
+    case "warning":
+      return <AlertTriangle size={24} />;
+    case "success":
+      return <CheckCircle size={24} />;
+    case "error":
+      return <AlertOctagon size={24} />;
+    default:
+      return <Info size={24} />;
+  }
+}
 
 function Toast({ children, variant }) {
-  const Icon = ICONS_BY_VARIANT[variant];
-  console.log("form.variant", variant);
+  // const Icon = ICONS_BY_VARIANT[form.variant];
+
   return (
     <div className={`${styles.toast} ${styles[variant]}`}>
-      <div className={styles.iconContainer}>
-        <Icon size={24} />
-      </div>
+      <div className={styles.iconContainer}>{handleIcon(variant)}</div>
       <p className={styles.content}>{children}</p>
       <button className={styles.closeButton}>
         <X size={24} />
--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -26,7 +26,7 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
-      <Toast variant={variant}>{message}</Toast>
+      <Toast variant={form.variant}>{form.message}</Toast>
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
