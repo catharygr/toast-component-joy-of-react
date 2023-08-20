93af0577081170532bfbf3a0e249a2a0af99f122
catharygr <103951447+catharygr@users.noreply.github.com>
2023-08-20 14:20:13 +0100

    Terminado la mitad del segundo componente

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import Button from "../Button";
+import Toast from "../Toast";
 import styles from "./ToastPlayground.module.css";
 
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
@@ -25,6 +26,7 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+      <Toast form={form} />
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
