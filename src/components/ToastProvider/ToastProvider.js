97b5c6f08766acc97a4346f5bc6967269df4bb62
catharygr <103951447+catharygr@users.noreply.github.com>
2023-08-21 17:01:30 +0100

    Creando Conetxt de toasts

---
+++ src/components/ToastProvider/ToastProvider.js
@@ -0,0 +1,16 @@
+import React from "react";
+// Crear un contexto
+const ToastContext = React.createContext();
+
+function ToastProvider({ children }) {
+  // Crear un estado para guardar los toasts
+  const [toasts, setToasts] = React.useState([]);
+
+  return (
+    <ToastContext.Provider value={[toasts, setToasts]}>
+      {children}
+    </ToastContext.Provider>
+  );
+}
+
+export { ToastProvider, ToastContext };
