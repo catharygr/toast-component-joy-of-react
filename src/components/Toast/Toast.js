8eefa93cd4f5bc70c041cc372c038cb9ed4f9be2
catharygr <103951447+catharygr@users.noreply.github.com>
2023-08-22 12:31:42 +0100

    Terminado el proyecto

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -11,34 +11,19 @@ import VisuallyHidden from "../VisuallyHidden";
 
 import styles from "./Toast.module.css";
 
-// const ICONS_BY_VARIANT = {
-//   notice: Info,
-//   warning: AlertTriangle,
-//   success: CheckCircle,
-//   error: AlertOctagon,
-// };
-
-function handleIcon(variant) {
-  switch (variant) {
-    case "notice":
-      return <Info size={24} />;
-    case "warning":
-      return <AlertTriangle size={24} />;
-    case "success":
-      return <CheckCircle size={24} />;
-    case "error":
-      return <AlertOctagon size={24} />;
-    default:
-      return <Info size={24} />;
-  }
-}
+const ICONS_BY_VARIANT = {
+  notice: Info,
+  warning: AlertTriangle,
+  success: CheckCircle,
+  error: AlertOctagon,
+};
 
 function Toast({ children, variant, closeToast, uuid }) {
-  // const Icon = ICONS_BY_VARIANT[form.variant];
+  const Icon = ICONS_BY_VARIANT[variant];
 
   return (
     <div className={`${styles.toast} ${styles[variant]}`}>
-      <div className={styles.iconContainer}>{handleIcon(variant)}</div>
+      <div className={styles.iconContainer}>{<Icon size={24} />}</div>
       <p className={styles.content}>{children}</p>
       <button onClick={() => closeToast(uuid)} className={styles.closeButton}>
         <X size={24} />
